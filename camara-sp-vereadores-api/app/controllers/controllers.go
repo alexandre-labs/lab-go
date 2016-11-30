5c00a58e77ca80be4c24b5c7b05f1a97c8b955a6
Alexandre <0x41e@protonmail.ch>
2016-11-30 01:16:14 -0200

    Adicionar função para obter detalhes de um vereador

--- camara-sp-vereadores-api/app/controllers/controllers.go
+++ camara-sp-vereadores-api/app/controllers/controllers.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 )
 import "github.com/alexandre/lab-go/camara-sp-vereadores-api/app/structs"
 
@@ -17,4 +19,20 @@ func GetAllVereadores(session *mgo.Session) []structs.Vereadores {
 	}
 
 	return vereadores
+}
+
+func GetVereador(session *mgo.Session, vereadorId string) (v structs.Vereador, err error) {
+
+	collection := session.DB("camara-sp-vereadores").C("vereadores")
+
+	vereador := structs.Vereador{}
+
+	err = collection.Find(bson.M{"id": vereadorId}).One(&vereador)
+	if err != nil {
+		log.Print(err)
+		log.Print("vereadorId: ", vereadorId)
+		return vereador, err
+	}
+
+	return vereador, err
 }
