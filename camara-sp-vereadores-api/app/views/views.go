c9324a1924ea1db955861a28b21cc27b58dca20b
Alexandre <0x41e@protonmail.ch>
2016-11-30 01:16:41 -0200

    Atualizar views e adicionaer handler 'Vereador'

--- camara-sp-vereadores-api/app/views/views.go
+++ camara-sp-vereadores-api/app/views/views.go
@@ -3,17 +3,18 @@ package views
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 	"net/http"
 )
 import "github.com/alexandre/lab-go/camara-sp-vereadores-api/app/controllers"
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	fmt.Fprintf(w, "Hello world!")
 }
 
-func Vereadores(w http.ResponseWriter, r *http.Request) {
+func Vereadores(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	w.Header().Set("Content-Type", "application/json")
 
@@ -28,4 +29,25 @@ func Vereadores(w http.ResponseWriter, r *http.Request) {
 	data, _ := json.Marshal(vereadores)
 
 	w.Write(data)
+}
+
+func Vereador(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	vereadorId := ps.ByName("id")
+
+	vereador, err := controllers.GetVereador(session, vereadorId)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		data, _ := json.Marshal(vereador)
+		w.Write(data)
+	}
 }
